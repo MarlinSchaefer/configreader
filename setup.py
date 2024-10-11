bf736d6b2b5567e2577a1c6348776df47114785b
Marlin Schäfer <mb.schaefer@gmx.de>
2024-10-11 11:44:32 +0200

    Updating install

--- setup.py
+++ setup.py
@@ -1,4 +1,11 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 if __name__ == "__main__":
-    setup()
+    setup(name="configreader",
+          version="1.0.0",
+          description="Configuration file reader with sub-sections and interprable values.",
+          license="MIT",
+          author="Marlin Benedikt Schäfer",
+          package_dir={"": "src"},
+          packages=find_packages(where="src"),
+          )
